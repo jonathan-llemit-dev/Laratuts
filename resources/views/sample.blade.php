b792927f2cdb8f75ee0d127cf0c9c2bfb3cc337a
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-01-17 23:43:24 +0800

    tailwind added

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Sample Page</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1 class="text-green-600 font-bold text-9xl">Hello World</h1>
 </body>
 </html>
